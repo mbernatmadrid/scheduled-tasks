08d02b3ccc6f1d5cf7ca6848666c799330a31783
mbernatmadrid <m.bernat.madrid@gmail.com>
2026-09-06 08:39:55 +0200

    Refactor birthday email script with alias imports
    Updated script with my own version of the program.

--- main.py
+++ main.py
@@ -6,33 +6,37 @@
 # See the solution video in the 100 Days of Python Course for explainations.
 
 
-from datetime import datetime
-import pandas
-import random
+import datetime as dt
+import pandas as pd
+import random as rd
 import smtplib
-import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+my_email = "m.bernat.madrid@gmail.com"
+password = "byeb flsb hpvh tyta"
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email, password=password)
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+now = dt.datetime.now()
+current_month = now.month
+current_day = now.day
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
+letters = []
+
+for number in range(1, 4):
+    with open(f"letter_templates/letter_{number}.txt", "r", encoding="utf-8") as file:
+        content = file.read()
+        letters.append(content)
+
+df = pd.read_csv("birthdays.csv")
+
+for (index, row) in df.iterrows():
+    if row.month == current_month and row.day == current_day:
+        random_letter = rd.choice(letters)
+        birthday_letter = random_letter.replace("[NAME]", row["name"])
         connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+            from_addr=my_email, to_addrs=row["email"], msg=f"Subject: Peluchito birthday letter\n\n{birthday_letter}")
+
+
+connection.close()
