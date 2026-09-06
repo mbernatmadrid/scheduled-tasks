72022f011e37ce8b5e5e173bbc9117eb307957f8
mbernatmadrid <m.bernat.madrid@gmail.com>
2026-09-06 08:52:54 +0200

    Change password variable to placeholder
    Updated password variable to use a placeholder.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import random as rd
 import smtplib
 
 my_email = "m.bernat.madrid@gmail.com"
-password = ""
+password = "MY_EMAIL"
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
 connection.login(user=my_email, password=password)
